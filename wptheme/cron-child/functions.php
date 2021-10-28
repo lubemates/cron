a6fc3124e124f7a2caee6ada781ce44c70f484b3
lubemates <93296368+lubemates@users.noreply.github.com>
2021-10-27 17:44:29 -0700

    Add files via upload

---
+++ wptheme/cron-child/functions.php
@@ -0,0 +1,6 @@
+﻿<?php
+add_action( 'wp_enqueue_scripts', 'cron_fn_enqueue_styles' );
+function cron_fn_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+?>
